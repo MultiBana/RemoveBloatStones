6afea4deb90772ffbf339fdeb6f7831dbd45a269
MultiBana <nirgeva98@gmail.com>
2024-03-23 18:08:12 +0200

    initial commit

---
+++ src/client/java/com/multibana/removebloatstones/RemoveBloatStonesClient.java
@@ -0,0 +1,10 @@
+package com.multibana.removebloatstones;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RemoveBloatStonesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/multibana/removebloatstones/mixin/ChunkSectionMixin.java
@@ -0,0 +1,34 @@
+package com.multibana.removebloatstones.mixin;
+
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.chunk.ChunkSection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ChunkSection.class)
+public abstract class ChunkSectionMixin {
+
+	@Shadow
+	public abstract BlockState setBlockState(int x, int y, int z, BlockState state, boolean lock);
+
+	@Inject(method = "setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;", at = @At("HEAD"), cancellable = true)
+	private void dontGenerate(int x, int y, int z, BlockState state, boolean lock, CallbackInfoReturnable<BlockState> cir){
+		if(state.isOf(Blocks.TUFF)){
+			cir.setReturnValue(setBlockState(x, y, z, Blocks.DEEPSLATE.getDefaultState(), lock));
+		}
+		else if(state.isOf(Blocks.GRANITE)){
+			cir.setReturnValue(setBlockState(x, y, z, Blocks.STONE.getDefaultState(), lock));
+		}
+		else if(state.isOf(Blocks.DIORITE)){
+			cir.setReturnValue(setBlockState(x, y, z, Blocks.STONE.getDefaultState(), lock));
+		}
+		else if(state.isOf(Blocks.ANDESITE)){
+			cir.setReturnValue(setBlockState(x, y, z, Blocks.STONE.getDefaultState(), lock));
+		}
+	}
+}
